4287877a650b45053fa626c857659ef2838fdc99
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-07-04 13:26:50 -0400

    ansible.sh adding password

--- roles/ansible.sh
+++ roles/ansible.sh
@@ -1,2 +1,2 @@
 component=$1
-ansible-playbook -i $component-dev.devopssz14.online, -e ansible_user=ec2-user -e env=dev -e role_name=$component expense.yml
+ansible-playbook -i $component-dev.devopssz14.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 env=dev -e role_name=$component expense.yml
