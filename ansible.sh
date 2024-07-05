a8e0d733e649324b55c06d05c8e151a6f5de9222
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-07-05 10:38:50 -0400

    ansible .sh changes

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
 component=$1
-ansible-playbooks -i $component-dev.devopssz14.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 env=dev -e role_name=$component expense.yml  -e
+ansible-playbook -i $component-dev.devopssz14.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev role_name=$component expense.yml -e
